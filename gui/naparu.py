12888f2fbffcc61ad5baf5099948fdc8a2d90a8b
PhoenixIO <lenov.yoga.tab@gmail.com>
2023-06-03 06:43:25 +0300

    include_links=false

--- gui/naparu.py
+++ gui/naparu.py
@@ -4,10 +4,9 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), os.path.pardir))
 
 from gpt4free import you
-import json
 
 prompt = sys.argv[1]
-response = you.Completion.create(prompt=prompt, detailed=True, include_links=True)
+response = you.Completion.create(prompt=prompt, detailed=True, include_links=False)
 
 print(response.text)
 sys.stdout.flush()
